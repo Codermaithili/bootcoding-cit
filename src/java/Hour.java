5d0f1f9ee0ce2b0c0473ff1b6de1a1003249e0d3
KDKCE <msdhage22@gmail.com>
2023-08-17 15:50:11 +0530

    .....

---
+++ src/java/Hour.java
@@ -0,0 +1,10 @@
+public class Hour {
+    public static void main(String[] args) {
+        int hours= 2;
+        int minutes= 5;
+        int seconds = minutes * 60;
+        int hourSeconds = hours * 60 * 60;
+        System.out.println("hours to seconds:"+ hourSeconds);
+        System.out.println("minutes to seconds :"+ seconds);
+    }
+}
